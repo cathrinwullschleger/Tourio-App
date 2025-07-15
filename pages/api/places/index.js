7cb5d9cf396cb8a41c88456a98bad2c16d0f5753
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-15 10:35:31 +0200

    add error handling (try ... catch)

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -2,26 +2,30 @@ import dbConnect from "@/db/connect.js";
 import Place from "@/db/models/Place.js";
 
 export default async function handler(request, response) {
-  await dbConnect();
-  console.log("Hallo");
-  if (request.method === "GET") {
-    const places = await Place.find();
+  try {
+    await dbConnect();
 
-    response.status(200).json(places);
-    return;
-  }
-
-  if (request.method === "POST") {
-    try {
-      const placeData = request.body;
-      console.log(placeData);
-      await Place.create(placeData);
-
-      response.status(201).json({ status: "Place added" });
-    } catch (error) {
-      response.status(400).json({ error: error.message });
+    if (request.method === "GET") {
+      const places = await Place.find();
+      return response.status(200).json(places);
     }
-  }
 
-  response.status(405).json({ status: "Method not allowed." });
+    if (request.method === "POST") {
+      try {
+        const placeData = request.body;
+        console.log(placeData);
+        await Place.create(placeData);
+
+        return response.status(201).json({ status: "Place added" });
+      } catch (error) {
+        return response.status(400).json({ error: error.message });
+      }
+    }
+
+    return response.status(405).json({ status: "Method not allowed." });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ status: "Server error", error: error.message });
+  }
 }
